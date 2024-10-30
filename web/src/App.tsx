fd6f780121b194c57eb1f5681877e9a1f92a549b
Lily Wheeler <lwheel16@jh.edu>
2024-10-30 18:46:03 -0400

    main componants assembled

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,29 @@
-import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Sidebar from "@/components/sidebar";
+import Feed from "@/components/feed";
+
+const DEBUG = false;
 
 function App() {
   return (
-    <div className="flex justify-center items-center text-3xl min-h-screen">
-      <Button>My Journal</Button>
+    <div className="flex min-h-dvh">
+      <div
+        className={cn("flex-1 min-w-14", {
+          "border-r-2": DEBUG,
+        })}
+      >
+        <Sidebar />
+      </div>
+      <div className="w-full max-w-md mx-auto md:max-w-lg">
+         <Feed />
+      </div>
+      <div
+        className={cn("flex-1", {
+          "border-l-2": DEBUG,
+        })}
+      >
+        {/* Placeholder for another sidebar */}
+      </div>
     </div>
   );
 }
---
+++ web/src/components/posts.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { PostType } from "@/data/types";
+import db from "@/data/db.json";
+import Post from "./post";
+
+const Posts = () => {
+  const [posts, setPosts] = useState<PostType[]>(db.posts);
+
+  return (
+    <div>
+      {posts
+        .sort((a, b) => (a.date > b.date ? -1 : 1))
+        .map((post) => (
+           <Post key={post.id} post={post} />
+        ))}
+    </div>
+  );
+};
+
+export default Posts;
