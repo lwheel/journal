a6369d23c63fcfa59966de1950bb56ecc452a57a
Lily Wheeler <lwheel16@jh.edu>
2024-10-30 17:09:11 -0400

    update with shadin

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div className="flex justify-center items-center text-3xl min-h-screen">
-      My Journal
+      <Button>My Journal</Button>
     </div>
   );
 }
